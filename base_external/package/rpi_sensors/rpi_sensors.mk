5b93fd03791c3b33c15335e5bc32e29acf09172e
msmouni <marwan-smouni@hotmail.fr>
2025-10-26 19:57:38 +0100

    Update rpi_sensors package + Add init script to start as daemon

--- base_external/package/rpi_sensors/rpi_sensors.mk
+++ base_external/package/rpi_sensors/rpi_sensors.mk
@@ -2,9 +2,11 @@
 # rpi_sensors.mk - Buildroot package for Raspberry Pi sensors
 ################################################################################
 
-RPI_SENSORS_VERSION = fb0ff5f5f71a45ce3f6f7d4992d98dfe65630ce3
+RPI_SENSORS_VERSION = fc1ddae7a0ddf7ba1f74afea78ad7bc05fc59b8b
 RPI_SENSORS_SITE = https://github.com/msmouni/ecen-5713-final-project-sensors.git
 RPI_SENSORS_SITE_METHOD = git
+
+RPI_SENSORS_DEPENDENCIES = sqlite
 
 define RPI_SENSORS_BUILD_CMDS
 	$(MAKE) -C $(@D) \
@@ -12,6 +14,9 @@ define RPI_SENSORS_BUILD_CMDS
 endef
 
 define RPI_SENSORS_INSTALL_TARGET_CMDS
+	# Create writable directory for the DB
+	mkdir -p $(TARGET_DIR)/var/lib/rpi_sensors_data
+
 	$(INSTALL) -D -m 0755 $(@D)/build/bin/rpi_sensors $(TARGET_DIR)/usr/bin/rpi_sensors
 endef
 
